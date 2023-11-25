5aadbf11314f3800d179fd354cf86809bf66bef4
Happyroots <90665513+Happyroots@users.noreply.github.com>
2023-11-25 16:19:33 +0800

    改用request方法

--- utils/is_Connected.py
+++ utils/is_Connected.py
@@ -1,8 +1,11 @@
-# import requests
+import requests
 import subprocess
 
 def get_status_ping():
-    r = subprocess.run('ping www.cnki.net -n 1',
+    # https://sunlogin.oray.com/
+    r = subprocess.run(
+                        'ping www.cnki.net -n 1',
+                        # 'ping https://cn.bing.com/search?pglt=515&q=cnki&cvid=d33960dfab8143d59caf4e9607bad3f7&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEEyBggCEEUYQTIGCAMQRRhBMgYIBBBFGDwyBggFEEUYPNIBCDEwNjJqMGoxqAIAsAIA&FORM=ANNTA1&PC=CNNDDB -n 1',
                        stdout=subprocess.PIPE,
                        stderr=subprocess.PIPE,
                        stdin= subprocess.PIPE,
@@ -10,26 +13,30 @@ def get_status_ping():
     # print(r.returncode)
     return r.returncode
 
-# def get_status(): #不好用
-#     login_html = "http://www.cnki.net"
-#     try:
-#         page_status = requests.get(login_html, timeout=1)
-#         response = page_status.status_code
-#     except requests.exceptions.Timeout:
-#         response = -1  # 设置超时时的状态码
-#     except requests.exceptions.RequestException:
-#         response = -2  # 设置其他错误时的状态码
-#     print("登录页面状态码:", response)
-#     return response
+def get_status_requests(login_html = "http://www.cnki.net"): #不好用
+    # login_html = "http://www.cnki.net"
+    login_html = "https://cn.bing.com/search?pglt=515&q=cnki&cvid=d33960dfab8143d59caf4e9607bad3f7&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARBFGEEyBggCEEUYQTIGCAMQRRhBMgYIBBBFGDwyBggFEEUYPNIBCDEwNjJqMGoxqAIAsAIA&FORM=ANNTA1&PC=CNNDDB"
+    try:
+        page_status = requests.get(login_html, timeout=1)
+        response = page_status.status_code
+    except requests.exceptions.Timeout:
+        response = -1  # 设置超时时的状态码
+    except requests.exceptions.RequestException:
+        response = -2  # 设置其他错误时的状态码
+    print("登录页面状态码:", response)
+    return response
 
 def is_Connected():
     connect = False
-    # get_status()
-    response = get_status_ping()
-    if(response == 1):
+    # response = get_status_ping()
+    # if(response == 1):
+    #     connect = False
+    # elif(response == 0):
+    #     connect = True
+    response = get_status_requests()
+    if(response == 200):
+            connect = True
+    elif(response == -1 or response == -2):
         connect = False
-    elif(response == 0):
-        connect = True
-    # print( is_Connected())
-
+        
     return connect
